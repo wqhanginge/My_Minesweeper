5d5b8d7f88a1e15f01fbb436048ddc0e97dbc423
Gee Wang <54838305+wqhanginge@users.noreply.github.com>
2020-07-04 15:25:46 +0800

    <Add Files>
    Changes:
    - Add source and resource files.
    - Add Visual Studio project files.
    - Update README.md.

---
+++ My_Minesweeper/resource.h
@@ -0,0 +1,93 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 Minesweeper_Resource.rc 使用
+//
+#define IDD_INFO                        101
+#define IDD_MAPAREA                     102
+#define IDD_MINE                        103
+#define IDD_TIME                        104
+#define IDR_MENU1                       106
+#define IDB_RESETBUTTON                 117
+#define IDB_FAIL                        118
+#define IDB_CLICK                       119
+#define IDB_NUM0                        120
+#define IDB_NUM1                        121
+#define IDB_NUM2                        122
+#define IDB_NUM3                        123
+#define IDB_NUM4                        124
+#define IDB_NUM5                        125
+#define IDB_NUM6                        126
+#define IDB_NUM7                        127
+#define IDB_NUM8                        128
+#define IDB_NUM9                        129
+#define IDB_NUMMINUS                    130
+#define IDB_TEST                        131
+#define IDB_SUCCESS                     131
+#define IDB_UNIT                        133
+#define IDB_UNITFLAG                    134
+#define IDB_UNITMARK                    135
+#define IDB_UNITNULL                    136
+#define IDB_UNIT1                       137
+#define IDB_UNIT2                       138
+#define IDB_UNIT3                       139
+#define IDB_UNIT4                       140
+#define IDB_UNIT5                       141
+#define IDB_UNIT6                       142
+#define IDB_UNIT7                       143
+#define IDB_UNIT8                       144
+#define IDB_UNITMINE                    145
+#define IDB_UNITBOMB                    146
+#define IDB_UNITWRONG                   147
+#define IDB_UNITMARKDOWN                148
+#define IDD_CUSTOM                      149
+#define IDD_GETNAME                     151
+#define IDD_RECORD                      153
+#define IDC_RESET                       1014
+#define IDC_MCH                         1021
+#define IDC_MCT                         1022
+#define IDC_MCU                         1023
+#define IDC_TNUMH                       1024
+#define IDC_TNUMT                       1025
+#define IDC_TNUMU                       1026
+#define IDC_EDITWIDTH                   1027
+#define IDC_WIDTH                       1028
+#define IDC_HEIGHT                      1029
+#define IDC_MINES                       1030
+#define IDC_EDITHEIGHT                  1031
+#define IDC_EDITMINES                   1032
+#define IDC_EDITNAME                    1033
+#define IDRESET                         1034
+#define IDC_TEXTNORMAL                  1035
+#define IDC_TEXTEASY                    1036
+#define IDC_TEXTHARD                    1037
+#define IDC_ETIME                       1038
+#define IDC_NTIME                       1039
+#define IDC_HTIME                       1040
+#define IDC_ENAME                       1041
+#define IDC_NNAME                       1042
+#define IDC_HNAME                       1043
+#define IDC_BUTTON1                     1044
+#define IDC_OK                          1044
+#define ID_GAME_START                   40003
+#define ID_GAME_EASY                    40004
+#define ID_GAME_NORMAL                  40005
+#define ID_GAME_HARD                    40006
+#define ID_GAME_CUSTOM                  40007
+#define ID_GAME_EXIT                    40008
+#define ID_Menu                         40009
+#define ID_GAME_MARK                    40010
+#define ID_GAME_BESTSCORES              40011
+#define ID_GAME_RECORD                  40012
+#define ID_HELP                         40013
+#define ID_HELP_ABOUT                   40014
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        157
+#define _APS_NEXT_COMMAND_VALUE         40015
+#define _APS_NEXT_CONTROL_VALUE         1045
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
