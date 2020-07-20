a33548237747fb732f93140ea7f0933d181f52b9
Gee Wang <54838305+wqhanginge@users.noreply.github.com>
2020-07-20 17:58:30 +0800

    <UI Update>
    Changes:
    - Remove MapArea Dialog, Info Dialog, Time Dialog and Mine Dialog.
    - Add new code to draw the window client area instead.
    - Remove MapUnit and InfoNum related bitmaps.
    - Update related macros and code.

--- My_Minesweeper/minesweeper_support.cpp
+++ My_Minesweeper/minesweeper_support.cpp
@@ -3,21 +3,19 @@
 HINSTANCE hInst;
 HWND hWnd;
 HMENU hMenu;
-HWND hInfo, hMapArea;
-HWND hMine, hTime;
 
-HBITMAP hbm_reset, hbm_click, hbm_fail, hbm_success;
-HBITMAP hbm_infonums[BM_INFONUMS_COUNT];
-HBITMAP hbm_unit, hbm_unitflag, hbm_unitmark;
-HBITMAP hbm_mapnums[BM_MAPNUMS_COUNT];
-HBITMAP hbm_unitmine, hbm_unitbomb, hbm_unitwrong, hbm_unitmarkdown;
-HBITMAP &hbm_unitdown = hbm_mapnums[0];
+HBITMAP hbm_resetb, hbm_click, hbm_fail, hbm_success;
 
 
-int lparamtopos(LPARAM lparam) {
-	POINTS p;
-	p = MAKEPOINTS(lparam);
-	return xytoindex(p.x / BM_MAPUNIT_SIZEM, p.y / BM_MAPUNIT_SIZEM);
+bool lparamispos(LPARAM lparam) {
+	POINTS p = MAKEPOINTS(lparam);
+	p = { p.x - MAPUNITSLEFT,p.y - MAPUNITSTOP };
+	return (p.x >= 0 && p.x < xtorleft(Game.width) && p.y >= 0 && p.y < ytortop(Game.height));
+}
+
+int lparamtoindex(LPARAM lparam) {
+	POINTS p = MAKEPOINTS(lparam);
+	return rpostoindex(p.x - MAPUNITSLEFT, p.y - MAPUNITSTOP);
 }
 
 int str2int(char *str, dword len, dword &x) {
@@ -41,147 +39,27 @@ int int2str(char *str, dword len, dword x) {
 	return ret;
 }
 
-void loadbitmaps() {
-	hbm_reset = LoadBitmap(hInst, MAKEINTRESOURCE(IDB_RESETBUTTON));
-	hbm_click = LoadBitmap(hInst, MAKEINTRESOURCE(IDB_CLICK));
-	hbm_fail = LoadBitmap(hInst, MAKEINTRESOURCE(IDB_FAIL));
-	hbm_success = LoadBitmap(hInst, MAKEINTRESOURCE(IDB_SUCCESS));
-	for (int i = 0; i < BM_INFONUMS_COUNT; i++)
-		hbm_infonums[i] = LoadBitmap(hInst, MAKEINTRESOURCE(IDB_NUM0 + i));
-	hbm_unit = LoadBitmap(hInst, MAKEINTRESOURCE(IDB_UNIT));
-	hbm_unitflag = LoadBitmap(hInst, MAKEINTRESOURCE(IDB_UNITFLAG));
-	hbm_unitmark = LoadBitmap(hInst, MAKEINTRESOURCE(IDB_UNITMARK));
-	for (int i = 0; i < BM_MAPNUMS_COUNT; i++)
-		hbm_mapnums[i] = LoadBitmap(hInst, MAKEINTRESOURCE(IDB_UNITNULL + i));
-	hbm_unitmine = LoadBitmap(hInst, MAKEINTRESOURCE(IDB_UNITMINE));
-	hbm_unitbomb = LoadBitmap(hInst, MAKEINTRESOURCE(IDB_UNITBOMB));
-	hbm_unitwrong = LoadBitmap(hInst, MAKEINTRESOURCE(IDB_UNITWRONG));
-	hbm_unitmarkdown = LoadBitmap(hInst, MAKEINTRESOURCE(IDB_UNITMARKDOWN));
-}
+void paintmap(HDC hdestdc, int muleft, int mutop) {
+	HDC hdcbuffer = CreateCompatibleDC(hdestdc);
+	HBITMAP hbmbuffer = CreateCompatibleBitmap(hdestdc, MAPUNITSIZE, MAPUNITSIZE);
+	SelectObject(hdcbuffer, hbmbuffer);
 
-void freebitmaps() {
-	DeleteObject(hbm_reset);
-	DeleteObject(hbm_click);
-	DeleteObject(hbm_fail);
-	DeleteObject(hbm_success);
-	for (int i = 0; i < BM_INFONUMS_COUNT; i++)
-		DeleteObject(hbm_infonums[i]);
-	DeleteObject(hbm_unit);
-	DeleteObject(hbm_unitflag);
-	DeleteObject(hbm_unitmark);
-	for (int i = 0; i < BM_MAPNUMS_COUNT; i++)
-		DeleteObject(hbm_mapnums[i]);
-	DeleteObject(hbm_unitmine);
-	DeleteObject(hbm_unitbomb);
-	DeleteObject(hbm_unitwrong);
-	DeleteObject(hbm_unitmarkdown);
-}
-
-int infow() {
-	switch (Game.mode) {
-	case EASY:		return IE_WM;
-	case NORMAL:	return IN_WM;
-	case HARD:		return IH_WM;
-	case CUSTOM:	return (Game.width * BM_MAPUNIT_SIZEM + DDLGEDGE);
-	default:		break;
-	}
-	return IE_WM;
-}
-
-int resetbuttonx() {
-	switch (Game.mode) {
-	case EASY:		return RESETBE_LEFTM;
-	case NORMAL:	return RESETBN_LEFTM;
-	case HARD:		return RESETBH_LEFTM;
-	case CUSTOM:	return ((Game.width * BM_MAPUNIT_SIZEM - RESETB_SIZE) / 2);
-	default:		break;
-	}
-	return RESETBE_LEFTM;
-}
-
-int timedlgx() {
-	switch (Game.mode) {
-	case EASY:		return TIMEE_LEFTM;
-	case NORMAL:	return TIMEN_LEFTM;
-	case HARD:		return TIMEH_LEFTM;
-	case CUSTOM:	return (Game.width * BM_MAPUNIT_SIZEM - TIME_ROFFSET);
-	default:		break;
-	}
-	return TIMEE_LEFTM;
-}
-
-int mapareaw() {
-	switch (Game.mode) {
-	case EASY:		return MAE_WM;
-	case NORMAL:	return MAN_WM;
-	case HARD:		return MAH_WM;
-	case CUSTOM:	return (Game.width * BM_MAPUNIT_SIZEM + DDLGEDGE);
-	default:		break;
-	}
-	return MAE_WM;
-}
-
-int mapareah() {
-	switch (Game.mode) {
-	case EASY:		return MAE_HM;
-	case NORMAL:	return MAN_HM;
-	case HARD:		return MAH_HM;
-	case CUSTOM:	return (Game.height * BM_MAPUNIT_SIZEM + DDLGEDGE);
-	default:		break;
-	}
-	return MAE_HM;
-}
-
-HWND *createstatics(HWND hparent) {
-	HWND *hstatic = new HWND[Game.size];
-	for (word i = 0; i < Game.size; i++) {
-		hstatic[i] = CreateWindowEx(0, TEXT("STATIC"), NULL,
-			WS_CHILD | WS_VISIBLE | SS_BITMAP,
-			(i % Game.width)*BM_MAPUNIT_SIZEM, (i / Game.width)*BM_MAPUNIT_SIZEM,
-			BM_MAPUNIT_SIZEM, BM_MAPUNIT_SIZEM,
-			hparent,
-			NULL,
-			hInst,
-			NULL);
-		SendMessage(hstatic[i], STM_SETIMAGE, IMAGE_BITMAP, (LPARAM)hbm_unit);
-	}
-	return hstatic;
-}
-
-void deletestatics(HWND **hstatic, word size) {
-	for (word i = 0; i < size; i++) DestroyWindow((*hstatic)[i]);
-	delete[] *hstatic;
-	*hstatic = nullptr;
-}
-
-void paintmap(HWND hstatic[]) {
 	for (word i = 0; i < Game.size; i++) {
 		if (!Game.mapchange[i]) continue;
-		switch (GETUNITSTATE(Game.map[i])) {
-		case UNITCOVER:
-			SendMessage(hstatic[i], STM_SETIMAGE, IMAGE_BITMAP, (LPARAM)hbm_unit);
-			break;
-		case UNITFLAG:
-			SendMessage(hstatic[i], STM_SETIMAGE, IMAGE_BITMAP, (LPARAM)hbm_unitflag);
-			break;
-		case UNITMARK:
-			SendMessage(hstatic[i], STM_SETIMAGE, IMAGE_BITMAP, (LPARAM)hbm_unitmark);
-			break;
-		case UNITUNCOV:
-			if (UNITISMINE(Game.map[i]))
-				SendMessage(hstatic[i], STM_SETIMAGE, IMAGE_BITMAP, (LPARAM)hbm_unitmine);
-			else
-				SendMessage(hstatic[i], STM_SETIMAGE, IMAGE_BITMAP, (LPARAM)hbm_mapnums[GETUNITMINES(Game.map[i])]);
-			break;
-		case UNITBOMB:
-			SendMessage(hstatic[i], STM_SETIMAGE, IMAGE_BITMAP, (LPARAM)hbm_unitbomb);
-			break;
-		case UNITWRONG:
-			SendMessage(hstatic[i], STM_SETIMAGE, IMAGE_BITMAP, (LPARAM)hbm_unitwrong);
-			break;
-		default:
-			break;
-		}
+		drawmapunitNB(hdcbuffer, 0, 0, i);
+		BitBlt(hdestdc, muleft + indextorleft(i), mutop + indextortop(i), MAPUNITSIZE, MAPUNITSIZE, hdcbuffer, 0, 0, SRCCOPY);
+	}
+	//remove "need update sign"
+	memset(Game.mapchange, 0, sizeof(bool)*Game.size);
+
+	DeleteObject(hbmbuffer);
+	DeleteObject(hdcbuffer);
+}
+
+void paintmapNB(HDC hdestdc, int muleft, int mutop) {
+	for (word i = 0; i < Game.size; i++) {
+		if (!Game.mapchange[i]) continue;
+		drawmapunitNB(hdestdc, muleft + indextorleft(i), mutop + indextortop(i), i);
 	}
 	//remove "need update sign"
 	memset(Game.mapchange, 0, sizeof(bool)*Game.size);
@@ -192,32 +70,32 @@ void paintmap(HWND hstatic[]) {
  *then change clicked units bitmap,
  *finally mark clicked units
  */
-void showclickdown(HWND hstatic[], Neighbor &poses) {
+void showclickdown(HDC hdestdc, int muleft, int mutop, Neighbor &indexes) {
 	for (word i = 0; i < 9; i++)
-		if (poses[i] >= 0 && poses[i] < (int)Game.size)
-			Game.mapchange[poses[i]] = false;
-	paintmap(hstatic);
+		if (indexes[i] >= 0 && indexes[i] < (int)Game.size)
+			Game.mapchange[indexes[i]] = false;
+	paintmap(hdestdc, muleft, mutop);
 	for (word i = 0; i < 9; i++) {
-		if (poses[i] < 0 || poses[i] >= (int)Game.size) continue;
-		if (GETUNITSTATE(Game.map[poses[i]]) == UNITCOVER)
-			PostMessage(hstatic[poses[i]], STM_SETIMAGE, IMAGE_BITMAP, (LPARAM)hbm_unitdown);
-		else if (GETUNITSTATE(Game.map[poses[i]]) == UNITMARK)
-			PostMessage(hstatic[poses[i]], STM_SETIMAGE, IMAGE_BITMAP, (LPARAM)hbm_unitmarkdown);
+		if (indexes[i] < 0 || indexes[i] >= (int)Game.size) continue;
+		if (GETUNITSTATE(Game.map[indexes[i]]) == UNITCOVER)
+			drawmuuncovbg(hdestdc, muleft + indextorleft(indexes[i]), mutop + indextortop(indexes[i]));
+		else if (GETUNITSTATE(Game.map[indexes[i]]) == UNITMARK)
+			drawmumark(hdestdc, muleft + indextorleft(indexes[i]), mutop + indextortop(indexes[i]), true);
 	}
 	for (word i = 0; i < 9; i++)
-		if (poses[i] >= 0 && poses[i] < (int)Game.size)
-			Game.mapchange[poses[i]] = true;
+		if (indexes[i] >= 0 && indexes[i] < (int)Game.size)
+			Game.mapchange[indexes[i]] = true;
 }
 
-void showclickdown(HWND hstatic[], int pos) {
-	if (pos < 0 || pos >= (int)Game.size) return;
-	Game.mapchange[pos] = false;
-	paintmap(hstatic);
-	if (GETUNITSTATE(Game.map[pos]) == UNITCOVER)
-		PostMessage(hstatic[pos], STM_SETIMAGE, IMAGE_BITMAP, (LPARAM)hbm_unitdown);
-	else if (GETUNITSTATE(Game.map[pos]) == UNITMARK)
-		PostMessage(hstatic[pos], STM_SETIMAGE, IMAGE_BITMAP, (LPARAM)hbm_unitmarkdown);
-	Game.mapchange[pos] = true;
+void showclickdown(HDC hdestdc, int muleft, int mutop, int index) {
+	if (index < 0 || index >= (int)Game.size) return;
+	Game.mapchange[index] = false;
+	paintmap(hdestdc, muleft, mutop);
+	if (GETUNITSTATE(Game.map[index]) == UNITCOVER)
+		drawmuuncovbg(hdestdc, muleft + indextorleft(index), mutop + indextortop(index));
+	else if (GETUNITSTATE(Game.map[index]) == UNITMARK)
+		drawmumark(hdestdc, muleft + indextorleft(index), mutop + indextortop(index), true);
+	Game.mapchange[index] = true;
 }
 
 void changechecked(byte GameMode) {
@@ -265,7 +143,7 @@ void changechecked(byte GameMode) {
 	}
 }
 
-void changemark(bool Mark){
+void changemark(bool Mark) {
 	MENUITEMINFO mii = { sizeof(MENUITEMINFO) };
 	mii.fMask = MIIM_STATE;
 	mii.fState = (Mark) ? MFS_CHECKED : MFS_UNCHECKED;
