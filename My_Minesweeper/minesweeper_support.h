30ce548123cc1bd1c31d806aeb37a163061b1a9e
Gee Wang <54838305+wqhanginge@users.noreply.github.com>
2020-07-20 17:58:29 +0800

    <UI Update>
    Changes:
    - Remove MapArea Dialog, Info Dialog, Time Dialog and Mine Dialog.
    - Add new code to draw the window client area instead.
    - Remove MapUnit and InfoNum related bitmaps.
    - Update related macros and code.

--- My_Minesweeper/minesweeper_support.h
+++ My_Minesweeper/minesweeper_support.h
@@ -11,68 +11,75 @@
 #include "resource.h"
 #include "minesweeper_defines.h"
 #include "minesweeper_game.h"
+#include "minesweeper_ui.h"
 
 #define WM_GAMERESET	(WM_APP + 0)		//-
 #define WM_GAMESUCCESS	(WM_APP + 1)		//-
 #define WM_GAMEFAIL		(WM_APP + 2)		//-
 #define WM_GAMESTART	(WM_APP + 3)		//use lparam as start position
-#define WM_GAMEMODECHG	(WM_APP + 4)		//use wparam as old mode, use lparam as old width(loword) and height(hiword)
-//#define WM_MENUMARK		(WM_APP + 5)		//use wparam as mark sign
-#define WM_MINECHG		(WM_APP + 6)		//-
-#define WM_SETMINE		(WM_APP + 7)		//use lparam as mines
-#define WM_TIMESTART	(WM_APP + 8)		//-
-#define WM_TIMESTOP		(WM_APP + 9)		//-
-#define WM_SETTIME		(WM_APP + 10)		//use lparam as time
-#define WM_CLICKDOWN	(WM_APP + 11)		//-
-#define WM_CLICKUP		(WM_APP + 12)		//-
+#define WM_GAMEMODECHG	(WM_APP + 4)		//use wparam as new mode, use lparam as new width, height and mines
+
+#define MAKECHGLPARAM(w, h, m)	((LPARAM)((((dword)(w) & 0xFF) | (((dword)(h) & 0xFF) << 8)) | (((dword)(m) & 0xFFFF) << 16)))
+#define GETCHGWIDTH(l)			((byte)((dword)(l) & 0xFF))
+#define GETCHGHEIGHT(l)			((byte)(((dword)(l) >> 8) & 0xFF))
+#define GETCHGMINES(l)			((word)(((dword)(l) >> 16) & 0xFFFF))
+
+#define ID_RESETB	5000
 
 extern HINSTANCE hInst;
 extern HWND hWnd;
 extern HMENU hMenu;
-extern HWND hInfo, hMapArea;
-extern HWND hMine, hTime;
 
-extern HBITMAP hbm_reset, hbm_click, hbm_fail, hbm_success;
-extern HBITMAP hbm_infonums[BM_INFONUMS_COUNT];
-extern HBITMAP hbm_unit, hbm_unitflag, hbm_unitmark;
-extern HBITMAP hbm_mapnums[BM_MAPNUMS_COUNT];
-extern HBITMAP hbm_unitmine, hbm_unitbomb, hbm_unitwrong, hbm_unitmarkdown;
-extern HBITMAP &hbm_unitdown;
+extern HBITMAP hbm_resetb, hbm_click, hbm_fail, hbm_success;
 
 
+//if a mouse position is inside the MapUnits area
+bool lparamispos(_In_ LPARAM laparm);
 //change a mouse position to map index
-int lparamtopos(LPARAM lparam);
+int lparamtoindex(_In_ LPARAM lparam);
 
 //transform between positive integer and c_string
 //return -1 if error
-int str2int(char *str, dword size, dword &x);
-template <dword size> int str2int(char(&str)[size], dword &x) { return str2int(str, size, x); }
+int str2int(_In_ char *str, _In_ dword size, _Out_ dword &x);
+template <dword size> inline int str2int(_In_ char(&str)[size], _Out_ dword &x)
+{ return str2int(str, size, x); }
+
 //return length of transformed c_string
-int int2str(char *str, dword size, dword x);
-template <dword size> int int2str(char(&str)[size], dword x) { return int2str(str, size, x); }
+int int2str(_Out_ char *str, _In_ dword size, _In_ dword x);
+template <dword size> inline int int2str(_Out_ char(&str)[size], _In_ dword x)
+{ return int2str(str, size, x); }
 
-//manage bitmaps
-void loadbitmaps();
-void freebitmaps();
-
-//get flexiable dialog's position
-int infow();
-int resetbuttonx();
-int timedlgx();
-int mapareaw();
-int mapareah();
-
-//manage Static Control in MapArea
-HWND *createstatics(HWND hparent);
-void deletestatics(HWND **hstatic, word size);
-
-void paintmap(HWND hstatic[]);
-void showclickdown(HWND hstatic[], Neighbor &poses);
-void showclickdown(HWND hstatic[], int pos);
+/* manage MapUnit in MapArea */
+//paint GameMap, use 'left' and 'top' as position zero of the whole map
+void paintmap(
+	_In_ HDC hdestdc,
+	_In_ int muleft,
+	_In_ int mutop
+);
+//paint GameMap directly, use 'left' and 'top' as position zero of the whole map
+void paintmapNB(
+	_In_ HDC hdestdc,
+	_In_ int muleft,
+	_In_ int mutop
+);
+//paint specific MapUnit directly, use 'left' and 'top' as position zero of the whole map
+void showclickdown(
+	_In_ HDC hdestdc,
+	_In_ int muleft,
+	_In_ int mutop,
+	_In_ Neighbor &indexes
+);
+//paint specific MapUnit directly, use 'left' and 'top' as position zero of the whole map
+void showclickdown(
+	_In_ HDC hdestdc,
+	_In_ int muleft,
+	_In_ int mutop,
+	_In_ int index
+);
 
 //change checked position in Menu
-void changechecked(byte GameMode);
-void changemark(bool Mark);
+void changechecked(_In_ byte GameMode);
+void changemark(_In_ bool Mark);
 
 //get best score info
 dword besttime();
@@ -80,7 +87,7 @@ char *bestname();
 
 //create a c_string to show time score
 //return length of the new c_string
-int maketimestr(char *buffer, int size, dword time, const char *timeunit = DEFTIMEUNIT);
+int maketimestr(_Out_ char *buffer, _In_ int size, _In_ dword time, _In_ const char *timeunit = DEFTIMEUNIT);
 template <dword size>
-int maketimestr(char(&buffer)[size], dword time, const char *timeunit = DEFTIMEUNIT)
+int maketimestr(_Out_ char(&buffer)[size], _In_ dword time, _In_ const char *timeunit = DEFTIMEUNIT)
 { return maketimestr(buffer, size, time, timeunit); }
