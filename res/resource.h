0d30627ed45fd5459362d12579d6ef64dbc135f4
wqhanginge <54838305+wqhanginge@users.noreply.github.com>
2020-10-02 15:35:39 +0800

    <Code Structure Change>
    Changes:
    - Incompatible changes to the data structure of the config(save) file
      and its path. Update the game saving and loading process accordingly.
    - New feature: Remember last window position.
    - New program icon.
    - Introduce <tchar.h> to handle encoding, and change the program to a
      Unicode version.
    - Optimize the reaction effect when clicking on the window area.
    - Modify and rename some functions and macros to improve their
      functionality and clarity.
    - Upgrade to Visual Studio 2019 and Windows 10 SDK.
    - Refactor the entire project directory and code structure for improved
      clarity.

---
+++ res/resource.h
@@ -0,0 +1,54 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 Resource.rc 使用
+//
+#define IDB_RESETB                      101
+#define IDB_CLICK                       102
+#define IDB_SUCCESS                     103
+#define IDB_FAIL                        104
+#define IDR_MENU1                       106
+#define IDI_ICON1                       114
+#define IDD_CUSTOM                      149
+#define IDD_GETNAME                     151
+#define IDD_RECORD                      153
+#define IDC_EDITWIDTH                   1027
+#define IDC_WIDTH                       1028
+#define IDC_HEIGHT                      1029
+#define IDC_MINES                       1030
+#define IDC_EDITHEIGHT                  1031
+#define IDC_EDITMINES                   1032
+#define IDC_EDITNAME                    1033
+#define IDRESET                         1034
+#define IDC_TEXTM                       1035
+#define IDC_TEXTJ                       1036
+#define IDC_TEXTS                       1037
+#define IDC_ETIME                       1038
+#define IDC_NTIME                       1039
+#define IDC_HTIME                       1040
+#define IDC_JNAME                       1041
+#define IDC_MNAME                       1042
+#define IDC_HNAME                       1043
+#define IDC_SNAME                       1043
+#define IDC_OK                          1044
+#define ID_GAME_JUNIOR                  40001
+#define ID_GAME_JUNIOR40002             40002
+#define ID_GAME_MIDDLE                  40003
+#define ID_GAME_SENIOR                  40004
+#define ID_GAME_CUSTOM                  40005
+#define ID_GAME_MARK                    40006
+#define ID_GAME_EXIT                    40007
+#define ID_GAME_START                   40008
+#define ID_GAME_RECORD                  40009
+#define IDD_HELP                        40010
+#define ID_HELP                         40011
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        116
+#define _APS_NEXT_COMMAND_VALUE         40012
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
