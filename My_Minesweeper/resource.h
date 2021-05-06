52fca099bcf659b55ee1b63bed86dcb0e9ead0d3
Gee Wang <54838305+wqhanginge@users.noreply.github.com>
2021-05-06 16:15:12 +0800

    <Bug Fix & UI Update>
    Fix:
    - Unstable mine generation process that generates an incorrect GameMap
      in some cases.
    - Dialogs do not close after clicking the default close button
      (top-right of the Dialog window).
    - Unexpected behavior that the main window can be maximized.
    
    Change:
    - Add a new Dialog for the 'About' message.
    - Improve the appearance of some Dialogs.

--- My_Minesweeper/resource.h
+++ My_Minesweeper/resource.h
@@ -8,9 +8,13 @@
 #define IDB_FAIL                        104
 #define IDR_MENU1                       106
 #define IDI_ICON1                       114
+#define IDD_DIALOG1                     117
+#define IDD_ABOUT                       117
 #define IDD_CUSTOM                      149
 #define IDD_GETNAME                     151
 #define IDD_RECORD                      153
+#define IDC_ABOUTTEXT                   1003
+#define IDC_ABOUTICON                   1004
 #define IDC_EDITWIDTH                   1027
 #define IDC_WIDTH                       1028
 #define IDC_HEIGHT                      1029
@@ -47,9 +51,9 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        117
+#define _APS_NEXT_RESOURCE_VALUE        119
 #define _APS_NEXT_COMMAND_VALUE         40013
-#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_CONTROL_VALUE         1005
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
