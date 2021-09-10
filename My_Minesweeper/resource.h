cf89076a9b5d360aa89472e8c909e7b042a08488
Gee Wang <54838305+wqhanginge@users.noreply.github.com>
2021-09-10 21:08:52 +0800

    <Bug Fix>
    Fix:
    - The main window may be shown outside of the Desktop in some cases.
    - The program cannot be closed from the Taskbar icon.
    
    Change:
    - Adjust the name and position of some resources.

--- My_Minesweeper/resource.h
+++ My_Minesweeper/resource.h
@@ -8,30 +8,28 @@
 #define IDB_FAIL                        104
 #define IDR_MENU1                       106
 #define IDI_ICON1                       114
-#define IDD_DIALOG1                     117
 #define IDD_ABOUT                       117
 #define IDD_CUSTOM                      149
 #define IDD_GETNAME                     151
 #define IDD_RECORD                      153
 #define IDC_ABOUTTEXT                   1003
 #define IDC_ABOUTICON                   1004
-#define IDC_EDITWIDTH                   1027
-#define IDC_WIDTH                       1028
-#define IDC_HEIGHT                      1029
-#define IDC_MINES                       1030
+#define IDC_WIDTH                       1027
+#define IDC_HEIGHT                      1028
+#define IDC_MINES                       1029
+#define IDC_EDITWIDTH                   1030
 #define IDC_EDITHEIGHT                  1031
 #define IDC_EDITMINES                   1032
 #define IDC_EDITNAME                    1033
 #define IDRESET                         1034
-#define IDC_TEXTM                       1035
-#define IDC_TEXTJ                       1036
+#define IDC_TEXTJ                       1035
+#define IDC_TEXTM                       1036
 #define IDC_TEXTS                       1037
-#define IDC_ETIME                       1038
-#define IDC_NTIME                       1039
-#define IDC_HTIME                       1040
+#define IDC_JTIME                       1038
+#define IDC_MTIME                       1039
+#define IDC_STIME                       1040
 #define IDC_JNAME                       1041
 #define IDC_MNAME                       1042
-#define IDC_HNAME                       1043
 #define IDC_SNAME                       1043
 #define IDC_OK                          1044
 #define ID_GAME_JUNIOR                  40001
@@ -43,9 +41,7 @@
 #define ID_GAME_EXIT                    40007
 #define ID_GAME_START                   40008
 #define ID_GAME_RECORD                  40009
-#define IDD_HELP                        40010
-#define ID_HELP                         40011
-#define ID_ABOUT                        40012
+#define ID_ABOUT                        40010
 
 // Next default values for new objects
 // 
