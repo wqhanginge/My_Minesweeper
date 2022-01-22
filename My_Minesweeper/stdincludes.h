06996046800e70c79347cced83709ad08f20c8c3
Gee Wang <54838305+wqhanginge@users.noreply.github.com>
2022-01-22 22:47:53 +0800

    <Hug C & UI Update>
    Changes:
    - Forget about C++ and switch to C.
    - Encapsulate the core functionality of right-click and move it to
      'gamecore.c'.
    - Change the font of all Dialogs to improve their appearance.
    - Add a copyright statement in all source code files and update several
      comments.
    - Update the 'About' message.

--- My_Minesweeper/stdincludes.h
+++ My_Minesweeper/stdincludes.h
@@ -1,28 +1,43 @@
+/*****************************************************************************\
+ *  My Minesweepper -- a classic minesweeper game
+ *  Copyright (C) 2020-2022 Gee W.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+\*****************************************************************************/
+
 #pragma once
 
+//disable uncommonly used APIs and reduce program size
+#ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
+#endif
+//use unicode in program
 #ifndef UNICODE
 #define UNICODE
 #endif
 
 #include <Windows.h>
+#include <stdlib.h>
 #include <tchar.h>
-#include <ctime>
-#include <memory>
+#include <time.h>
+#include <stdbool.h>
 #include "resource.h"
-#include <strsafe.h>
 #pragma comment(lib, "Version.lib")
-using namespace std;
 
 
 typedef unsigned char byte;
 typedef unsigned short word;
 typedef unsigned dword;
 
-
-#ifdef UNICODE
-#define tcsscanf_s	swscanf_s
-#else
-#define tcsscanf_s	swscanf_s
-#endif
-#define tcsprintf StringCchPrintf
+#define nullptr NULL
